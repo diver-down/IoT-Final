b705e9c99fa5ac5978d0de046beb9da1ae796656
diver-down <cdrew2@stevens.edu>
2018-12-06 15:40:30 -0500

    Fixed cross-validation!  Cleaning code up next...

--- final.py
+++ final.py
@@ -76,23 +76,32 @@ Your cross-validation should have as many data points as the regression plot.
 The array x should have two columns: index and CPU usage. The target y is temperature.
 """
 tColumn = t.reshape(-1,1)  # this reshapes array (t) from a row into a column
-dataStackCPU = np.column_stack((t,cpu)) # combining index array (t) with CPU array
-dataStackTemp = np.column_stack((t,temp)) # combining index array (t) with CPU array
+dataStackCPU = np.column_stack((tColumn,cpu)) # combining index array (t) with CPU array
+dataStackTemp = np.column_stack((tColumn,temp)) # combining index array (t) with CPU array
 lr = linear_model.LinearRegression()
 #  print('Number of instances: %d' % (cpu.shape[0]))
 # dataStack = (np.column_stack((cpu[:150],temp[:150])))
 #dataStack = ()
 #print("Data Stack:")
 #print(dataStack)
-predicted = cross_val_predict(lr, dataStackCPU, dataStackTemp, cv=3)
+predicted = cross_val_predict(lr, dataStackCPU, dataStackTemp, cv=10)
+print(predicted)
+print(type(predicted))
 
 fig, ax = plt.subplots()  # unsure if this CV is done properly
-
-# ax.scatter(temp[:150], predicted)
-ax.plot([0, 100], [0, 100], 'k-', lw=2)
+#ax.scatter(predicted[:,0], predicted[:,1])
+print(temp)
+print(type(temp))
+print("$%@^&*^&*%^&*&%^&^%&")
+print(predicted)
+print(type(predicted))
+ax.scatter(temp, predicted[:,1])
+#ax.matshow(predicted)
+# ax.scatter(temp,predicted)
+#ax.plot([35, 90], [35, 90], 'k-', lw=2)
 # alternate:
-# ax.plot([temp.min(), temp.max()], [temp.min(), temp.max()], 'k-', lw=2)
-ax.set_ylim(bottom = 0, top = 100)
+ax.plot([temp.min(), temp.max()], [temp.min(), temp.max()], 'k-', lw=2)
+#ax.set_ylim(bottom = 0, top = 100)
 plt.title('Cross-Validation Predition for Temperature')
 plt.xlabel('Measured')
 plt.ylabel('Predicted')
